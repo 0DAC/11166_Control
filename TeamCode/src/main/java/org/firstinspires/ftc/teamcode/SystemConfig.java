c97486523d08f1cc43c64db9be9e3b3e73ced829
0DAC <mzhang21@lawrenceville.org>
2019-10-25 19:45:17 -0400

    Basic Movement

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SystemConfig.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Contains system configuration variables for the robot
+ */
+public class SystemConfig {
+
+    public String[] left_motors = new String[] {"left_1", "left_2"};
+    public String[] right_motors = new String[] {"right_1", "right_2"};
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motors/ControlSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motors/ControlSystem.java
@@ -4,8 +4,11 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.SystemConfig;
+
 @TeleOp(name = "Gamepad Manual Control", group = "Control")
 public class ControlSystem extends LinearOpMode {
+    SystemConfig sys = new SystemConfig();
     MotorGroup left, right;
     Servo arm;
     private static double MAX_DIST = 5;
@@ -19,39 +22,51 @@ public class ControlSystem extends LinearOpMode {
         left = new MotorGroup();
         right = new MotorGroup();
         // arm = hardwareMap.get(Servo.class, "arm");
-        controller = new Gamepad();
         MotorGroup left = new MotorGroup();
         MotorGroup right = new MotorGroup();
 
-        left.init(hardwareMap, telemetry, "left_side", new String[] {"left_1", "left_2"});
-        right.init(hardwareMap, telemetry, "right_side", new String[] {"right_1", "right_2"});
+        left.init(hardwareMap, telemetry, "left_side", sys.left_motors);
+        right.init(hardwareMap, telemetry, "right_side", sys.right_motors);
+
+        waitForStart();
 
         while (opModeIsActive()) {
             // A = vertical arm move up
-            if (controller.a) {
+            if (gamepad1.a) {
+                telemetry.addData("KeyPress:", "A");
                 //    arm.setPosition(arm.getPosition()+MAX_DIST);
             }
             // Y = vertical arm move down
-            else if (controller.y) {
+            else if (gamepad1.y) {
+                telemetry.addData("KeyPress:", "A");
                 //    arm.setPosition(arm.getPosition()-MAX_DIST);
             }
             // right_bumper = increase movement speed
-            if (controller.right_bumper) {
+            if (gamepad1.right_bumper) {
+                telemetry.addData("KeyPress:", "A");
                 //    left.changeSpeed(DELTA_SPEED);
                 //    right.changeSpeed(DELTA_SPEED);
             }
             // left_bumper = decrease movement speed
-            else if (controller.left_bumper) {
+            else if (gamepad1.left_bumper) {
+
+                telemetry.addData("KeyPress:", "A");
                 //    left.changeSpeed(-DELTA_SPEED);
                 //    right.changeSpeed(-DELTA_SPEED);
             }
             // left joystick = linear movement
-            left.setPower(controller.left_stick_y * MAX_DIST + Math.max(0, controller.left_stick_x) * MAX_DIST);
-            right.setPower(-(controller.left_stick_y * MAX_DIST + Math.min(0, controller.left_stick_x) * MAX_DIST));
+            //telemetry.addData("Left Power:", controller.left_stick_y * MAX_DIST + Math.max(0, controller.left_stick_x) * MAX_DIST);
+            //telemetry.addData("Right Power:", -(controller.left_stick_y * MAX_DIST + Math.min(0, controller.left_stick_x) * MAX_DIST));
+
+            telemetry.addData("Joystick Values", String.format("(" + gamepad1.left_stick_x + ", " + gamepad1.left_stick_y + ")"));
+
+            left.setPower(-(gamepad1.left_stick_y * MAX_DIST - gamepad1.left_stick_x * MAX_DIST));
+            right.setPower((gamepad1.left_stick_y * MAX_DIST + gamepad1.left_stick_x * MAX_DIST));
             // right joystick = in-place rotation
 
             //left.update();
             //right.update();
+            telemetry.update();
         }
     }
 }
