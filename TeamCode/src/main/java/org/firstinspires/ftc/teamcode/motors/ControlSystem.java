b9d950f35d0b246741593c6eacbcffc3da297145
0DAC <mzhang21@lawrenceville.org>
2019-10-25 20:07:57 -0400

    Added speed change, rotation in place, halved intial velocity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motors/ControlSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motors/ControlSystem.java
@@ -11,8 +11,8 @@ public class ControlSystem extends LinearOpMode {
     SystemConfig sys = new SystemConfig();
     MotorGroup left, right;
     Servo arm;
-    private static double MAX_DIST = 5;
-    private static double DELTA_SPEED = 0.1;
+    private static double MAX_DIST = 2;
+    private static double DELTA_SPEED = 0.5;
     private Gamepad controller;
 
     @Override
@@ -43,26 +43,23 @@ public class ControlSystem extends LinearOpMode {
             }
             // right_bumper = increase movement speed
             if (gamepad1.right_bumper) {
-                telemetry.addData("KeyPress:", "A");
-                //    left.changeSpeed(DELTA_SPEED);
-                //    right.changeSpeed(DELTA_SPEED);
+                MAX_DIST -= DELTA_SPEED;
             }
             // left_bumper = decrease movement speed
             else if (gamepad1.left_bumper) {
-
-                telemetry.addData("KeyPress:", "A");
-                //    left.changeSpeed(-DELTA_SPEED);
-                //    right.changeSpeed(-DELTA_SPEED);
+                MAX_DIST -= DELTA_SPEED;
             }
             // left joystick = linear movement
-            //telemetry.addData("Left Power:", controller.left_stick_y * MAX_DIST + Math.max(0, controller.left_stick_x) * MAX_DIST);
-            //telemetry.addData("Right Power:", -(controller.left_stick_y * MAX_DIST + Math.min(0, controller.left_stick_x) * MAX_DIST));
+            if (gamepad1.right_stick_x == 0 && gamepad1.right_stick_y == 0) {
+                telemetry.addData("Left Joystick Values", String.format("(" + gamepad1.left_stick_x + ", " + gamepad1.left_stick_y + ")"));
 
-            telemetry.addData("Joystick Values", String.format("(" + gamepad1.left_stick_x + ", " + gamepad1.left_stick_y + ")"));
-
-            left.setPower(-(gamepad1.left_stick_y * MAX_DIST - gamepad1.left_stick_x * MAX_DIST));
-            right.setPower((gamepad1.left_stick_y * MAX_DIST + gamepad1.left_stick_x * MAX_DIST));
-            // right joystick = in-place rotation
+                left.setPower(-(gamepad1.left_stick_y * MAX_DIST - gamepad1.left_stick_x * MAX_DIST));
+                right.setPower((gamepad1.left_stick_y * MAX_DIST + gamepad1.left_stick_x * MAX_DIST));
+            }
+            else {
+                left.setPower(gamepad1.right_stick_x* MAX_DIST);
+                right.setPower(gamepad1.right_stick_x * MAX_DIST);
+            }
 
             //left.update();
             //right.update();
