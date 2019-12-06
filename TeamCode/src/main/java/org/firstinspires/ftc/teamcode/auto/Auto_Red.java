600056f9b5ce1edd50cb1fae45cacb91169b060d
0DAC <mzhang21@lawrenceville.org>
2019-12-06 18:34:11 -0500

    Working on DogeCV and integrating other code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Auto_Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Auto_Red.java
@@ -1,14 +1,10 @@
 package org.firstinspires.ftc.teamcode.auto;
 
-import android.app.Dialog;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.teamcode.util.GetSide;
 
 @Autonomous(name="Auto_Red", group="DogeCV")
 public class Auto_Red extends LinearOpMode {
