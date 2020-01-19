b6ee739faaa7bafd1aa7310863735c09f95788a9
anushka-chintamaneni <56006003+anushka-chintamaneni@users.noreply.github.com>
2020-01-19 11:45:55 -0500

    Update StrafePIDTuner.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bot/StrafePIDTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bot/StrafePIDTuner.java
@@ -4,12 +4,13 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.teamcode.HOMAR.PIDTuner;
 import org.firstinspires.ftc.teamcode.bot.components.Robot;
 
 @Autonomous(name = "Strafe PID Tuning", group = "auto")
 public class StrafePIDTuner extends LinearOpMode {
     private final double speed = 0.7;
-    private final long time_per = 1000;
+    private final long time_per = 3000;
 
     Robot bot;
 
@@ -35,8 +36,65 @@ public class StrafePIDTuner extends LinearOpMode {
         telemetry.update();
         bot.power_drive(Math.PI / 2, speed, 0);
         bot.print_encoder_vals(telemetry);
+
+        bot.power_drive(Math.PI / 2, speed, 0);
+        bot.print_encoder_vals(telemetry);
+        telemetry.update();
+        bot.pause(time_per);
+        bot.power_drive(Math.PI, speed, 0);
+        bot.print_encoder_vals(telemetry);
+        telemetry.update();
+        bot.pause(time_per);
+        bot.power_drive(3 * Math.PI / 2, speed, 0);
+        bot.print_encoder_vals(telemetry);
+        telemetry.update();
+        bot.pause(time_per);
+        bot.power_drive(0, speed, 0);
+        bot.print_encoder_vals(telemetry);
+        telemetry.update();
+        bot.power_drive(Math.PI / 2, speed, 0);
+        bot.print_encoder_vals(telemetry);
+
+
+        bot.power_drive(Math.PI / 2, speed, 0);
+        bot.print_encoder_vals(telemetry);
+        telemetry.update();
+        bot.pause(time_per);
+        bot.power_drive(Math.PI, speed, 0);
+        bot.print_encoder_vals(telemetry);
+        telemetry.update();
+        bot.pause(time_per);
+        bot.power_drive(3 * Math.PI / 2, speed, 0);
+        bot.print_encoder_vals(telemetry);
+        telemetry.update();
+        bot.pause(time_per);
+        bot.power_drive(0, speed, 0);
+        bot.print_encoder_vals(telemetry);
+        telemetry.update();
+        bot.power_drive(Math.PI / 2, speed, 0);
+        bot.print_encoder_vals(telemetry);
+
+
+        bot.power_drive(Math.PI / 2, speed, 0);
+        bot.print_encoder_vals(telemetry);
+        telemetry.update();
+        bot.pause(time_per);
+        bot.power_drive(Math.PI, speed, 0);
+        bot.print_encoder_vals(telemetry);
+        telemetry.update();
+        bot.pause(time_per);
+        bot.power_drive(3 * Math.PI / 2, speed, 0);
+        bot.print_encoder_vals(telemetry);
+        telemetry.update();
+        bot.pause(time_per);
+        bot.power_drive(0, speed, 0);
+        bot.print_encoder_vals(telemetry);
+        telemetry.update();
+        bot.power_drive(Math.PI / 2, speed, 0);
+        bot.print_encoder_vals(telemetry);
         telemetry.update();
 
 
     }
 }
+
